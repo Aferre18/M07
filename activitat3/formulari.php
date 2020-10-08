484dee0760edc8b7f5260ed4b5318e301e0c2a13
Aferre18 <aferrec@fp.insjoaquimmir.cat>
2020-10-08 17:07:36 +0200

    Formulario final

--- activitat3/formulari.php
+++ activitat3/formulari.php
@@ -1,8 +1,19 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
    //Processar les dades
-} else {
-   //Pintar el formulari
+    echo "Has ecrito esto en text: $_REQUEST[mytext].<br>";
+    echo "Has seleccionado la:  $_REQUEST[myradio].<br>";
+
+    if(isset ($_REQUEST["mycheckbox"])){
+        foreach ($_REQUEST["mycheckbox"] as $respuesta){
+        echo "Has selecionado la checkbox: $respuesta <br>";
+        }
+    }
+
+    echo "Has seleccionado el grupo $_REQUEST[myselect].<br>";
+    echo "Has escrito en el recuadro: $_REQUEST[mytextarea].<br>";  
+    }   else {
+    //Pintar el formulari
 
 
 
